8af0831f8802daf0fc7eb9b3059e7c804ee1e5fe
YK12321 <y01.kaushal@gmail.com>
2025-10-31 14:45:27 -0400

    Refactor LEDMatrix data structure: replace smiling face with raw, Sobel, and Canny images

--- 6/ledMatrix.py
+++ 6/ledMatrix.py
@@ -14,26 +14,16 @@ MSBFIRST = 2
 dataPin   = 11      # DS Pin of 74HC595(Pin14)
 latchPin  = 13      # ST_CP Pin of 74HC595(Pin12)
 clockPin = 15       # SH_CP Pin of 74HC595(Pin11)
-pic = [0x1c,0x22,0x51,0x45,0x45,0x51,0x22,0x1c]  # data of smiling face
-data = [     # data of "0-F"
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, # " "
-    0x00, 0x00, 0x3E, 0x41, 0x41, 0x3E, 0x00, 0x00, # "0"
-    0x00, 0x00, 0x21, 0x7F, 0x01, 0x00, 0x00, 0x00, # "1"
-    0x00, 0x00, 0x23, 0x45, 0x49, 0x31, 0x00, 0x00, # "2"
-    0x00, 0x00, 0x22, 0x49, 0x49, 0x36, 0x00, 0x00, # "3"
-    0x00, 0x00, 0x0E, 0x32, 0x7F, 0x02, 0x00, 0x00, # "4"
-    0x00, 0x00, 0x79, 0x49, 0x49, 0x46, 0x00, 0x00, # "5"
-    0x00, 0x00, 0x3E, 0x49, 0x49, 0x26, 0x00, 0x00, # "6"
-    0x00, 0x00, 0x60, 0x47, 0x48, 0x70, 0x00, 0x00, # "7"
-    0x00, 0x00, 0x36, 0x49, 0x49, 0x36, 0x00, 0x00, # "8"
-    0x00, 0x00, 0x32, 0x49, 0x49, 0x3E, 0x00, 0x00, # "9"
-    0x00, 0x00, 0x3F, 0x44, 0x44, 0x3F, 0x00, 0x00, # "A"
+rawpic = [0, 31, 31, 31, 0, 0, 0, 0]
+sobelpic = [31, 255, 31, 255, 255, 255, 255, 31]
+cannypic = [105, 81, 139, 80, 100, 73, 136, 41]
+data = [     # data
+    rawpic, # "binarypic"
+    sobelpic, # "sobelpic"
+    cannypic, # "cannypic"
     0x00, 0x00, 0x7F, 0x49, 0x49, 0x36, 0x00, 0x00, # "B"
+    0x00, 0x00, 0x46, 0x49, 0x49, 0x31, 0x00, 0x00  # "S"
     0x00, 0x00, 0x3E, 0x41, 0x41, 0x22, 0x00, 0x00, # "C"
-    0x00, 0x00, 0x7F, 0x41, 0x41, 0x3E, 0x00, 0x00, # "D"
-    0x00, 0x00, 0x7F, 0x49, 0x49, 0x41, 0x00, 0x00, # "E"
-    0x00, 0x00, 0x7F, 0x48, 0x48, 0x40, 0x00, 0x00, # "F"
-    0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, # " "
 ]
 def setup():
     GPIO.setmode(GPIO.BOARD)    # use PHYSICAL GPIO Numbering
@@ -52,26 +42,10 @@ def shiftOut(dPin,cPin,order,val):
 
 def loop():
     while True:
-        for j in range(0,500): # Repeat enough times to display the smiling face a period of time
-            x=0x80
-            for i in range(0,8):
-                GPIO.output(latchPin,GPIO.LOW)
-                shiftOut(dataPin,clockPin,MSBFIRST,pic[i]) #first shift data of line information to first stage 74HC959
+        for j in range(0,500): # Repeat enough times to display the patterns for a while
+            """Display each entry in data for 10 seconds, with a 0.5 second blank between alternations, and display B, S, C for only 2 seconds each
+            Display B before binary, S before sobel, etc."""
 
-                shiftOut(dataPin,clockPin,MSBFIRST,~x) #then shift data of column information to second stage 74HC959
-                GPIO.output(latchPin,GPIO.HIGH) # Output data of two stage 74HC595 at the same time
-                time.sleep(0.001) # display the next column
-                x>>=1
-        for k in range(0,len(data)-8): #len(data) total number of "0-F" columns
-            for j in range(0,20): # times of repeated displaying LEDMatrix in every frame, the bigger the "j", the longer the display time.
-                x=0x80      # Set the column information to start from the first column
-                for i in range(k,k+8):
-                    GPIO.output(latchPin,GPIO.LOW)
-                    shiftOut(dataPin,clockPin,MSBFIRST,data[i])
-                    shiftOut(dataPin,clockPin,MSBFIRST,~x)
-                    GPIO.output(latchPin,GPIO.HIGH)
-                    time.sleep(0.001)
-                    x>>=1
 def destroy():
     GPIO.cleanup()
 if __name__ == '__main__':  # Program entrance
